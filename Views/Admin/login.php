985f69d2b4956e40bf7226aca85bc6386d003dd0
Tinsae Gizachew <tigial3535@gmail.com>
2018-01-12 02:41:13 +0300

    simple

--- Views/Admin/login.php
+++ Views/Admin/login.php
@@ -29,7 +29,7 @@ if ( $_SERVER[ 'REQUEST_METHOD' ] == 'POST' ) {
 					$userData = Unique::getUserData();
 					if ( Input::get( 'email' ) === $userData->email && hash( 'sha256', Input::get( 'password' ) ) === $userData->password ) {
 						Session::put( 'user', $userData->id );
-							Redirect::to( 'index.php);
+							Redirect::to( 'index.php');
 
 					} else {
 
