afb7cd745ab31860c705b8b0fde8b41f711baf3b
Tinsae Gizachew <tigial3535@gmail.com>
2018-01-11 23:12:34 +0300

    simple commit

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if (Session::exists('user')) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>University Management System</title>
+    <title>University Management Systemmmm</title>
     <link rel="shortcut icon" type="image/x-icon" href="Views/assets/images/icon.png" />
 
     <!--  for bootstrap  -->
