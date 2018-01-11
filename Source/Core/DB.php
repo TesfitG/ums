4d3b725a4719a34e3527549fc0368500484200c2
Tinsae Gizachew <tigial3535@gmail.com>
2018-01-12 01:50:01 +0300

    major change in the db.php file

--- Source/Core/DB.php
+++ Source/Core/DB.php
@@ -1,8 +1,6 @@
 <?php  namespace App\Core;
 //this class is responsible for all CRUD operations for this project.
 use PDO;
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
 class DB
 {
     private static $_instance = null;
@@ -10,20 +8,58 @@ class DB
 	
     private function __construct()
     {
-		$dbname='gcp_560d7f4ddd1bd4129f05';
-		$username='bc1b42e7e30580';
-		$password='67a767d6';
+		session_start();
+		$dbname='';
+		$username='';
+		$password='';
+		if(isset($_SESSION['link']))
+		{
+			switch($_SESSION['link'])
+			{
+				case 'astu': 
+				$dbname = 'gcp_560d7f4ddd1bd4129f05'; 
+				$username='bc1b42e7e30580';
+				$password='67a767d6';
+				break;
+				
+				case 'aau': 
+				$dbname = 'gcp_7a6e18880e1cd5299b68'; 
+				$username='bf794cad1dacca';
+				$password='87b7008b';
+				break;
+				
+				case 'aastu': 
+				$dbname = 'gcp_9436d56f0606144ebf97'; 
+				$username='b8e83f022c3dd7';
+				$password='4403f4e0';
+				break;
+				
+				case 'du': 
+				$dbname = 'gcp_a4a8800ef254dfac2b96';
+				$username='bb1367cc04f65d';
+				$password='b30cb857';
+				break;
+				
+				default: 
+				$dbname = 'gcp_560d7f4ddd1bd4129f05'; 
+				$username='bc1b42e7e30580';
+				$password='67a767d6';
+				break;
+			}
+		}
+		else
+		{
+			$dbname = 'gcp_560d7f4ddd1bd4129f05'; 
+				$username='bc1b42e7e30580';
+				$password='67a767d6';
+		}
 		
-			
-		
-        try {		
-            $this->_pdo = new PDO('mysql:host=104.196.163.17;dbname=gcp_560d7f4ddd1bd4129f05', 'bc1b42e7e30580', '67a767d6' );
-        
-			$status = $this->_pdo->getAttribute(PDO::ATTR_CONNECTION_STATUS);
-			print(status);
-			
-			
-        } catch (PDOException $e) {
+        try 
+		{		
+            $this->_pdo = new PDO('mysql:host=us-cdbr-gcp-east-01.cleardb.net;dbname='.$dbname, $username, $password );
+        } 
+		catch (PDOException $e) 
+		{
             die($e->getMessage());
         }
     }
@@ -31,57 +67,57 @@ class DB
     //create object for this class by using this function.
     public static function getDB()
     {
-		error_reporting(E_ALL);
-		ini_set('display_errors', '1');
+		session_start();
 		$dbname='';
 		$username='';
 		$password='';
-		if(!isset($_SESSION['link']))
+		if(isset($_SESSION['link']))
 		{
-			$_SESSION['link'] = 'astu';
-		}
-		
-		//	switch($_SESSION['link'])
-			//{
-			//	case 'astu': 
+			switch($_SESSION['link'])
+			{
+				case 'astu': 
 				$dbname = 'gcp_560d7f4ddd1bd4129f05'; 
 				$username='bc1b42e7e30580';
 				$password='67a767d6';
-			//	break;
+				break;
 				
-				//case 'aau': 
-				//$dbname = 'gcp_7a6e18880e1cd5299b68'; 
-				//$username='bf794cad1dacca';
-				//$password='87b7008b';
-				//break;
+				case 'aau': 
+				$dbname = 'gcp_7a6e18880e1cd5299b68'; 
+				$username='bf794cad1dacca';
+				$password='87b7008b';
+				break;
 				
-				//case 'aastu': 
-				//$dbname = 'gcp_9436d56f0606144ebf97'; 
-				////$username='b8e83f022c3dd7';
-				//$password='4403f4e0';
-				//break;
+				case 'aastu': 
+				$dbname = 'gcp_9436d56f0606144ebf97'; 
+				$username='b8e83f022c3dd7';
+				$password='4403f4e0';
+				break;
 				
-				//case 'du': 
-				//$dbname = 'gcp_a4a8800ef254dfac2b96';
-				//$username='bb1367cc04f65d';
-				//$password='b30cb857';
-				//break;
+				case 'du': 
+				$dbname = 'gcp_a4a8800ef254dfac2b96';
+				$username='bb1367cc04f65d';
+				$password='b30cb857';
+				break;
 				
-				//default: 
-				//$dbname = 'gcp_560d7f4ddd1bd4129f05'; 
-				//$username='bc1b42e7e30580';
-				//$password='67a767d6';
-				//break;
-			//}
+				default: 
+				$dbname = 'gcp_560d7f4ddd1bd4129f05'; 
+				$username='bc1b42e7e30580';
+				$password='67a767d6';
+				break;
+			}
+		}
+		else
+		{
+			$dbname = 'gcp_560d7f4ddd1bd4129f05'; 
+			$username='bc1b42e7e30580';
+			$password='67a767d6';
+		}
 		
-		
-        if (!isset(self::$_instance)) {
-			
-			//self::$_instance = new PDO('mysql:host=127.0.0.1;dbname='.$dbname, 'root', '' );
-            
-			self::$_instance = new PDO('mysql:host=104.196.163.17;dbname=gcp_560d7f4ddd1bd4129f05', 'bc1b42e7e30580', '67a767d6' );
+        if (!isset(self::$_instance))
+		{
+			//self::$_instance = new PDO('mysql:host=127.0.0.1;dbname='.$dbname, 'root', '' );    
+			self::$_instance = new PDO('mysql:host=us-cdbr-gcp-east-01.cleardb.net;dbname='.$dbname, $username, $password );
         }
-		
         return self::$_instance;
     }
 
