c80d1e210946686d424a28d130ff91cbba978059
Tinsae Gizachew <tigial3535@gmail.com>
2018-01-11 23:16:02 +0300

    is debugging

--- Views/Admin/login.php
+++ Views/Admin/login.php
@@ -8,29 +8,18 @@ use App\Core\Validation;
 session_start();
 //print_r($_POST);exit();
 
-echo '<script language="javascript">';
-			echo 'window.alert("welcome alert");';
-			echo '</script>';}
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-	echo '<script language="javascript">';
-			echo 'window.alert("it is ofcourse post");';
-			echo '</script>';}
     if (Input::exists('post')){
         if (Validation::valid(Input::get('email')) && Validation::valid(Input::get('password'))){
-			echo '<script language="javascript">';
-			echo 'window.alert("valid input");';
-			echo '</script>';}
             if (Validation::emailValidation(Input::get('email'))){
                 if (!Unique::checkUser(Input::get('email'))){
                     $userData = Unique::getUserData();
                     if (Input::get('email') === $userData->email && hash('sha256', Input::get('password')) === $userData->password){
                         Session::put('user', $userData->id);
-						echo '<script language="javascript">';
-						echo 'window.alert("unable to redirect men");';
-						echo '</script>';}
-
                         Redirect::to('index.php');
-						
                     }else{
                         Session::put('error', 'Email or password not valid. please try again.');
                         Redirect::to('../../index.php');
@@ -53,8 +42,4 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     }
 }else{
     Redirect::to('../../index.php');
-				echo '<script language="javascript">';
-			echo 'window.alert("redirecting to main index.php");';
-			echo '</script>';}
-
 }
