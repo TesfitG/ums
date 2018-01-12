ce8742591539f1bc4b233da3c180b1e05ceb16dd
Tinsae Gizachew <tigial3535@gmail.com>
2018-01-13 00:19:49 +0300

    added university name in admin index 2

--- Views/Admin/logout.php
+++ Views/Admin/logout.php
@@ -1,24 +1,29 @@
 <?php
 require_once "../../vendor/autoload.php";
-use App\Core\Session;
-use App\Core\Redirect;
+use App\ Core\ Session;
+use App\ Core\ Redirect;
 session_start();
 
 // Deleting student registration No of 'student info page'.
-if (Session::exists('regNo')){
-    Session::delete('regNo');
+if ( Session::exists( 'regNo' ) ) {
+	Session::delete( 'regNo' );
 }
 
 // Deleting teachers id of 'Course Assign to teacher' page.
-if (Session::exists('teacher')){
-    Session::delete('teacher');
+if ( Session::exists( 'teacher' ) ) {
+	Session::delete( 'teacher' );
 }
 
-if (Session::exists('user')){
-    Session::delete('user');
-    Redirect::to('../../index.php');
-}else{
-    Redirect::to('../../index.php');
+if ( Session::exists( 'user' ) ) {
+	Session::delete( 'user' );
+
+	if ( isset( $_SESSION[ 'link' ] ) ) {
+		$uuu = $_SESSION[ 'link' ];
+	} else {
+		$uuu = 'astu';
+	}
+	Redirect::to( '../../index.php?univ=' . $uuu );
+
+} else {
+	Redirect::to( '../../index.php' );
 }
-
-
